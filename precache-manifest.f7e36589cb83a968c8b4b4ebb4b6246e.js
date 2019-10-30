26092dd1cb4d8513f5e2353dcb1403b7887b5b2f
Zeinab Gamal <zeinab.gamal512@gmail.com>
2019-10-30 20:23:34 +0200

    Updates

---
+++ precache-manifest.f7e36589cb83a968c8b4b4ebb4b6246e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ed6518b7c87db07cebd4c2251cdc19",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "d6440826109f952c40ea",
+    "url": "/React-app/static/css/main.4df16cec.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/React-app/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "d6440826109f952c40ea",
+    "url": "/React-app/static/js/main.559c61f9.chunk.js"
+  },
+  {
+    "revision": "d704105685e9a50e8fe8",
+    "url": "/React-app/static/js/runtime~main.a7bc867e.js"
+  }
+]);
